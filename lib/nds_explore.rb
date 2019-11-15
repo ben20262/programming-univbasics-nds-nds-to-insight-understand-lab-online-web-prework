49c7149af1a6b544f36734a117feedbb0f3d66e6
Benjamin Small <ben20262@aim.com>
2019-11-15 00:37:14 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 # Call the method directors_database to retrieve the NDS
-directors_database()
+
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
@@ -9,6 +9,7 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
+  nds = directors_database()
   array = []
   nds.each do |name|
     array << name[:movies][:title]
