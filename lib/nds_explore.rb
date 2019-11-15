185153e30021d0fa8a91a4d799e431f028aa965f
Benjamin Small <ben20262@aim.com>
2019-11-15 00:38:13 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,7 @@ end
 def print_first_directors_movie_titles
   nds = directors_database()
   array = []
-  nds.each do |name|
+  nds.keys.each do |name|
     array << name[:movies][:title]
   end
   array
